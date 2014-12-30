b8fd0bfe10464bff490f1e454dc58ad252fdd5ba
Jared Mulconry <j.mulconry@atomizergames.com>
2014-12-30 21:47:51 +1100

    Added a simple, temporary test case for dense map construction from two ranges.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "MyMallocator.hpp"
 #include <type_traits>
 #include "vector.hpp"
+#include <vector>
 
 void ConstructorTest();
 void AssignmentTest();
@@ -25,6 +26,12 @@ template class custom_std::vector < bool, allocator_type<bool> >;
 int main()
 {
 	std::cout.sync_with_stdio(false);
+
+	std::vector<int> keys{ 1, 5, 7, 3, 6, 77, 11, 5, 2, 9 };
+	std::vector<int> values{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	custom_containers::DenseMap<int, int> dm{ std::begin(keys), std::end(keys),
+										std::begin(values), std::end(values) };
 
 	ConstructorTest();
 	AssignmentTest();
